7635ed38e17c93e715004dd23d7ad84103d25e38
zainab-rafea <zainab2108676@miuegypt.edu.eg>
2023-03-24 02:22:15 +0200

    adding in contactus validation

--- contactus.js
+++ contactus.js
@@ -37,35 +37,79 @@
 //         return false;
 //     }
 // }
-let namein=document.querySelector("[name='cname']");
-let phin=document.querySelector("[name='cphone']");
-let emailin=document.querySelector("[name='cmail']");
-let mesin=document.querySelector("[name='cmes']");
+// let namein=document.querySelector("[name='cname']");
+// let phin=document.querySelector("[name='cphone']");
+// let emailin=document.querySelector("[name='cmail']");
+// let mesin=document.querySelector("[name='cmes']");
 
 
 
-document.forms[0].onsubmit=function(c){
-let namev=false;
-let phv=false;
-let emailv=false;
-let mesv=false;
+// document.forms[0].onsubmit=function(c){
+// let namev=false;
+// let phv=false;
+// let emailv=false;
+// let mesv=false;
  
-if(namein.value !==""){
-   namev=true;
-}
-if(phin.value !==""){
-    phv=true;
- }
- if(emailin.value !==""){
-    emailv=true;
- }
- if(mesin.value !==""){
-    mesv=true;
- }
+// if(namein.value !==""){
+//    namev=true;
+// }
+// if(phin.value !==""){
+//     phv=true;
+//  }
+//  if(emailin.value !==""){
+//     emailv=true;
+//  }
+//  if(mesin.value !==""){
+//     mesv=true;
+//  }
 
 
-if(namev===false || phv===false || emailv=== false || mesv===false){
-    c.preventDefault();
-}
+// if(namev===false || phv===false || emailv=== false || mesv===false){
+//     c.preventDefault();
+// }
 
-}
+//}
+function validate() {
+   let namein = document.forms.contactForm.cname.value;
+   let phin = document.forms.contactForm.cphone.value;
+   let emailin = document.forms.contactForm.cmail.value;
+   let mesin = document.forms.contactForm.cmes.value;
+   //window.alert('s'+mesin+'m');
+ 
+   if (namein == "") {
+    document.getElementById("label1").style.visibility = 'visible';
+   }
+   else{
+    document.getElementById("label1").style.visibility = 'hidden';
+   }
+   if (phin == "") {
+    document.getElementById("label2").style.visibility = 'visible';
+   }
+   else{
+    document.getElementById("label2").style.visibility = 'hidden';
+   }
+   if (emailin == "") {
+    document.getElementById("label3").style.visibility = 'visible';
+   }
+   else{
+    document.getElementById("label3").style.visibility = 'hidden';
+   }
+   if (mesin == "") {
+ 
+    document.getElementById("label5").style.visibility = 'visible';
+   }
+   else{
+    document.getElementById("label5").style.visibility = 'hidden';
+   }
+   if(phin.length!=11 && phin.length!=""){
+    document.getElementById("label6").style.visibility = 'visible';
+   }
+   else{
+    document.getElementById("label6").style.visibility = 'hidden';
+   }
+ 
+  if(namein == ""||phin == ""||emailin == ""||mesin == ""||phin.length!=11){
+    return false;
+  }
+  
+ }
